1d492d842ae50786324edc48a38fbf89c7604af6
Mohammed <mohammed@MOH-kali.MOH.com>
2026-06-16 02:38:32 +0300

    Implement the skeleton of home screen UI and navigation

--- vdownloader/lib/main.dart
+++ vdownloader/lib/main.dart
@@ -1,121 +1,181 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+final TextEditingController urlController = TextEditingController();
 void main() {
-  runApp(const MyApp());
+  runApp(app());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class app extends StatefulWidget {
+  State<app> createState() => _appState();
+}
 
-  // This widget is the root of your application.
+class _appState extends State<app> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            flexibleSpace: Stack(
+              children: [
+                Positioned(child: Image.asset("assets/LOGO.png"), width: 200),
+              ],
             ),
-          ],
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.history_rounded),
+                iconSize: 35,
+                tooltip: "tes123",
+                color: const Color.fromARGB(255, 255, 255, 255),
+              ),
+            ],
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "Download"),
+                Tab(text: "Quaue"),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.deepPurple),
+                      color: const Color.fromARGB(15, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    width: 325,
+                    height: 50,
+                    margin: EdgeInsets.only(top: 15),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: urlController,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: "Paste a Video or URL link....",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(
+                                horizontal: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 5),
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(40, 148, 90, 248),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: IconButton(
+                              iconSize: 20,
+                              onPressed: () async {
+                                ClipboardData? data = await Clipboard.getData(
+                                  Clipboard.kTextPlain,
+                                );
+
+                                if (data != null) {
+                                  urlController.text = data.text ?? '';
+                                }
+                              },
+                              icon: Icon(
+                                Icons.content_paste_rounded,
+                                color: Colors.deepPurpleAccent,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 20),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(0, 5),
+                          blurRadius: 35,
+                          spreadRadius: 10,
+                        ),
+                      ],
+                      border: Border.all(
+                        width: 5,
+                        color: Color.fromARGB(255, 148, 90, 248),
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: const Color.fromARGB(255, 148, 90, 248),
+                    ),
+                    width: 325,
+                    height: 50,
+                    child: TextButton(
+                      onPressed: () {
+                        debugPrint("keep working ,untill you Marry Malak ");
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.download_rounded, color: Colors.white),
+                          SizedBox(width: 10),
+                          Text(
+                            "Download",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.construction_rounded,
+                      color: Colors.grey,
+                      size: 64,
+                    ),
+
+                    SizedBox(height: 16),
+
+                    Text(
+                      "Coming in Future Releases",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    SizedBox(height: 8),
+
+                    Text(
+                      "This feature is under development.",
+                      style: TextStyle(color: Colors.grey, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
